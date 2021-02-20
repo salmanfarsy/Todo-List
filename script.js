26470a0386459fbf19d7ced0a85b01d01b470271
salmanfarsy <blieveburn007@gmail.com>
2021-02-20 17:53:54 +0600

    Logic is working needs hover effect and a lot of css

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const addbtn = document.querySelector('button');
-const inputBox = document.querySelector('input');
+const addbtn = document.querySelector('#add');
+const inputBox = document.querySelector('#input');
 const body = document.querySelector('body');
+const closebtn = document.querySelector('#close');
+const text = document.querySelector('input');
+const ul = document.querySelector('ul');
 
 addbtn.addEventListener('click', show);
-body.addEventListener('click', hide);
-
+closebtn.addEventListener('click', hide);
+text.addEventListener('keypress', check);
 
 function show(){
     inputBox.classList.remove('hide');
@@ -12,5 +15,36 @@ function show(){
 
 function hide(){
     inputBox.classList.add('hide');
-    console.log('body')
+  
+};
+
+function check(x){
+  if(x.keyCode === 13){
+    generate();
+    text.value = '';
+  }
 }
+
+function generate(){
+const  newLi = document.createElement('li');
+  const li = ul.appendChild(newLi);
+  li.textContent = text.value;
+  li.addEventListener('click', line.bind(li));
+  btn(li);
+};
+
+function line(){
+  this.classList.toggle('line');
+}
+
+function btn(x){
+  const newbtn = document.createElement('button');
+ const dltbtn= x.appendChild(newbtn);
+ dltbtn.textContent = 'Delete';
+ dltbtn.classList.add('delete');
+ dltbtn.addEventListener('click', dltNow.bind(null, x))
+}
+
+function dltNow(y){
+  y.remove(this)
+};
