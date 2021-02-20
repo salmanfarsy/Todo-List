29ab27c40ffaf659357849e6f0adaccbb78e0448
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-02-20 15:21:00 +0600

    working on the button

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert('connected');
+const addbtn = document.querySelector('button');
+const inputBox = document.querySelector('input');
+const body = document.querySelector('body');
+
+addbtn.addEventListener('click', show);
+body.addEventListener('click', hide);
+
+
+function show(){
+    inputBox.classList.remove('hide');
+};
+
+function hide(){
+    inputBox.classList.add('hide');
+    console.log('body')
+}
